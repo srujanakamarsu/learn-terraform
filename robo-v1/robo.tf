dd08ecea93248fd5d46b83d1f2f4690e135d03a4
srujanakamarsu <srujanamythree@gmail.com>
2023-08-10 11:52:37 +0530

    robo.tf servers and dns records updated

--- robo-v1/robo.tf
+++ robo-v1/robo.tf
@@ -6,7 +6,15 @@ resource "aws_instance" "frontend" {
   tags = {
     Name = "frontend"
   }
-} 
+}
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -16,6 +24,14 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -25,6 +41,14 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -34,6 +58,14 @@ resource "aws_instance" "redis" {
     Name = "redis"
   }
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -43,6 +75,14 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -52,6 +92,14 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
 resource "aws_instance" "mysql" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -61,6 +109,14 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -70,6 +126,14 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -79,6 +143,14 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -88,3 +160,11 @@ resource "aws_instance" "payment" {
     Name = "payment"
   }
 }
+resource "aws_route53_record" "payment" {
+  zone_id = "Z02452941A9F33HFSFSY2"
+  name    = "frontend-dev.vyshu.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
