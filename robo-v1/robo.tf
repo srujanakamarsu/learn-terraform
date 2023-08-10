13f7b93838b48c758be1eda454859d5f4c95cdaf
srujanakamarsu <srujanamythree@gmail.com>
2023-08-10 11:27:04 +0530

    security

--- robo-v1/robo.tf
+++ robo-v1/robo.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = "sg-0af3e5a83f971329a"
 
   tags = {
     Name = "frontend"
